05fa6c365e239e0b0d78952b5077cadb9fbde2b1
Guilherme Sousa Oliveira <guilhermesousa1110@gmail.com>
2023-05-30 10:57:02 -0300

    projeto

---
+++ Controllers/EquipeController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using projeto_gamer_mvc.Infra;
+
+namespace projeto_gamer_mvc.Controllers
+{
+    [Route("[controller]")]
+    public class EquipeController : Controller
+    {
+        private readonly ILogger<EquipeController> _logger;
+
+        public EquipeController(ILogger<EquipeController> logger)
+        {
+            _logger = logger;
+        }
+
+        Context c = new Context();
+
+        [Route("Listar")]
+        public IActionResult Index()
+        {
+            ViewBag.Equipe = c.Equipe.ToList();
+
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+
+
+    }
+}
---
+++ Infra/Context.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using projeto_gamer_mvc.Models;
+
+namespace projeto_gamer_mvc.Infra
+{
+    public class Context : DbContext
+    {
+        public Context()
+        {
+        }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = NOTE09-S15; Initial Catalog = gamerManha; User Id = sa; pwd = Senai@134; TrustServerCertificate = true");
+            }
+        }
+
+        public DbSet<Jogador> Jogador {get;set;}
+
+        public DbSet<Equipe> Equipe {get;set;}
+    }
+}
