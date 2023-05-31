146063b7b8ea30eec541b41b39c343b56692166b
Guilherme Sousa <94999182+GSolivier@users.noreply.github.com>
2023-05-31 10:52:47 -0300

    Projeto

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using projeto_gamer_mvc.Infra;
+using projeto_gamer_mvc.Models;
 
 namespace projeto_gamer_mvc.Controllers
 {
@@ -29,6 +24,57 @@ namespace projeto_gamer_mvc.Controllers
             
             return View();
         }
+
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            //instância do objeto Equipe
+            Equipe novaEquipe = new Equipe();
+
+
+            //atribuição dos valores recebidos pelo usuário
+            novaEquipe.Nome = form["Nome"].ToString();
+            
+
+             if (form.Files.Count > 0)
+            {
+                var file = form.Files[0];
+
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+
+
+                }
+
+                //gerar o caminho completo ate o caminho do arquivo
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                novaEquipe.Imagem = file.FileName;
+            }
+
+            else{
+                novaEquipe.Imagem = "padrao";
+
+            }
+
+            //adiciona o valor na tabela do banco de dados
+            c.Equipe.Add(novaEquipe);
+
+            //Salva as alterações na tabela do banco de dados
+            c.SaveChanges();
+
+            //Retorna para o local chamando a rota de listar (método Index)
+            return LocalRedirect("~/Equipe/Listar");
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
