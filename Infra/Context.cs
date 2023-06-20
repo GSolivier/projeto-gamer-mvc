499af8563f30b81b149c32045aa456ecd3e228fe
Guilherme Sousa <94999182+GSolivier@users.noreply.github.com>
2023-06-20 09:09:45 -0300

    Estilização do perfil do jogador e agora na aba de jogadores foi adicionado um foreach que separa todos os jogadores pelas equipes

--- Infra/Context.cs
+++ Infra/Context.cs
@@ -15,15 +15,15 @@ namespace projeto_gamer_mvc.Infra
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // if (!optionsBuilder.IsConfigured)
-            // {
-            //     optionsBuilder.UseSqlServer("Data Source = NOTE09-S15; Initial Catalog = gamerManha; User Id = sa; pwd = Senai@134; TrustServerCertificate = true");
-            // }
-
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Data Source = Guilherme; Initial Catalog = gamerManha; User Id = sa; pwd = Senai@134; TrustServerCertificate = true");
+                optionsBuilder.UseSqlServer("Data Source = NOTE09-S15; Initial Catalog = gamerManha; User Id = sa; pwd = Senai@134; TrustServerCertificate = true");
             }
+
+            // if (!optionsBuilder.IsConfigured)
+            // {
+            //     optionsBuilder.UseSqlServer("Data Source = Guilherme; Initial Catalog = gamerManha; User Id = sa; pwd = Senai@134; TrustServerCertificate = true");
+            // }
             
             // if (!optionsBuilder.IsConfigured)
             // {
